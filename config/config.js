586ca859a86ac2a7644a6f8df776719e1f5c1827
Tarlan Abdullayev <52969357+tarlanhub@users.noreply.github.com>
2021-04-19 03:21:35 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   IS_PROD: process.env.NODE_ENV === 'production',
   NODE_ENV: process.env.NODE_ENV,
-  PORT: "process.env.PORT",
+  PORT: process.env.PORT,
   HOST: "localhost",
   DB_USERNAME: "tarlan",
   DB_PASSWORD: "tarlan98",
